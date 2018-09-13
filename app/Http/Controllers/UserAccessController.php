3c057b28c5c0129efb9ba628308ff501f07037d1
sagar seth <sagar12jan@gmail.com>
2018-09-13 17:38:20 +0530

    added the registration api

--- app/Http/Controllers/UserAccessController.php
+++ app/Http/Controllers/UserAccessController.php
@@ -18,30 +18,33 @@ class UserAccessController extends Controller
         
     }
 
-     public function register(Request $request)
+    
+
+    public function register(Request $request)
     {  
        $data = json_decode(file_get_contents('php://input'));
        $new_score = new UserAccessService();
        $userexist = $new_score->get_userdata($data->phone);
        if($userexist != '0')
        {
-       $new_score->name=$data->name;
-       $new_score->email=$data->email;
-       $new_score->dob=$data->dob;
-       $new_score->contact_no=$data->phone;
-       $new_score->gender=$data->gender;
-       if($new_score->save())
-       {
-         $resp = array('status' =>'1' ,'data'=>$new_score,'message'=>'user registered' );    
-       }
+           $new_score->name=$data->name;
+           $new_score->email=$data->email;
+           $new_score->dob=$data->dob;
+           $new_score->contact_no=$data->phone;
+           $new_score->gender=$data->gender;
+           if($new_score->save())
+           {
+            $resp = array('status' =>'1' ,'data'=>$new_score,'message'=>'user registered' );    
+           }
+           else
+           {
+            $resp = array('status' =>'0' ,'data'=>$new_score,'message'=>'user not registered' );    
+           }
+        }
        else
        {
-        $resp = array('status' =>'0' ,'data'=>$new_score,'message'=>'user not registered' );    
+            $resp = array('status' =>'0' ,'data'=>$new_score,'message'=>'user already registered' );
        }
-        
-       }else
-       $resp = array('status' =>'0' ,'data'=>$new_score,'message'=>'user already registered' );
-       
        return response()->json($resp);
     }
     //
